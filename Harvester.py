1752acfee1643c23f9c3cc351e3f210d3832e10e
Bobby G Beach III <me@treybea.ch>
2018-05-08 03:42:59 -0400

    Fixed
    Repaired Issues with solving captchas

--- Harvester.py
+++ Harvester.py
@@ -2,7 +2,7 @@
 # Stay Based Youngins....
 
 from selenium import webdriver
-import time, getpass
+import time, getpass, selenium
 from selenium.webdriver.chrome.options import Options
 
 
@@ -14,7 +14,8 @@ class harvest:
         self.googleemail = gmail
         self.googlepass = gpass
         self.chrome = webdriver.Chrome()
-        self.htmlcode = "<html><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><head><script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' type='text/javascript'></script><script>(function() { if (!window['___grecaptcha_cfg']) { window['___grecaptcha_cfg'] = {}; }; if (!window['___grecaptcha_cfg']['render']) { window['___grecaptcha_cfg']['render'] = 'onload'; }; window['__google_recaptcha_client'] = true; var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.gstatic.com/recaptcha/api2/v1520231465640/recaptcha__en.js'; var elem = document.querySelector('script[nonce]'); var nonce = elem && (elem['nonce'] || elem.getAttribute('nonce')); if (nonce) { po.setAttribute('nonce', nonce); } var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script> <title>Captcha Harvester</title> <style type='text/css'> body { margin: 1em 5em 0 5em; font-family: sans-serif; }fieldset { display: inline; padding: 1em; } </style> </head><body> <center> <h3>Captcha Token Harvester</h3> <h5>HTML by: @pxtvr</h5> <h5>Python by: @Cosm00_</h5> <form action='http://serveriphere:5000/solve' method='post'> <fieldset> <div class='g-recaptcha' data-sitekey='sitekeygoeshere' data-callback='sub'></div> <p><input type='submit' value='Submit' id='submit' style='color: #ffffff;background-color: #3c3c3c;border-color: #3c3c3c;display: inline-block;margin-bottom: 0;font-weight: normal;text-align: center;vertical-align: middle;-ms-touch-action: manipulation;touch-action: manipulation;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 8px 12px;font-size: 15px;line-height: 1.4;border-radius: 0;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;'></p> </fieldset> </form> <fieldset> <h5 style='width: 10vh;'><a style='text-decoration: none;' href='http://serveriphere:5000/json' target='_blank'>Usable Tokens</a></h5></fieldset> </center><script> function sub() { document.getElementById('submit').click(); } </script> </body></html>".replace('sitekeygoeshere', self.sitekey).replace('serveriphere', self.serverip)
+        self.htmlcode = "<html><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><head><script type='text/javascript' src='https://www.google.com/recaptcha/api.js'></script><script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' type='text/javascript'></script> <title>Captcha Harvester</title> <style type='text/css'> body{margin: 1em 5em 0 5em; font-family: sans-serif;}fieldset{display: inline; padding: 1em;}</style></head><body> <center> <h3>Captcha Token Harvester</h3> <h5>HTML by: @pxtvr</h5> <h5>Python by: @Cosm00_</h5> <form action='http://serveriphere:5000/solve' method='post'> <fieldset> <div class='g-recaptcha' data-sitekey='sitekeygoeshere' data-callback='sub'></div><p> <input type='submit' value='Submit' id='submit' style='color: #ffffff;background-color: #3c3c3c;border-color: #3c3c3c;display: inline-block;margin-bottom: 0;font-weight: normal;text-align: center;vertical-align: middle;-ms-touch-action: manipulation;touch-action: manipulation;cursor: pointer;background-image: none;border: 1px solid transparent;white-space: nowrap;padding: 8px 12px;font-size: 15px;line-height: 1.4;border-radius: 0;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;'> </p></fieldset> </form> <fieldset> <h5 style='width: 10vh;'> <a style='text-decoration: none;' href='http://serveriphere:5000/json' target='_blank'>Usable Tokens</a> </h5> </fieldset> </center> <script>function sub(){document.getElementById('submit').click();}</script> </body></html>".replace('sitekeygoeshere',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          self.sitekey).replace('serveriphere', self.serverip)
     def signin(self):
         self.chrome.get('https://accounts.google.com/signin/v2')
         while True:
@@ -61,14 +62,20 @@ class harvest:
         self.chrome.get('https://www.youtube.com/watch?v=ZAyvEft9MIs')
     def solve(self):
         self.chrome.get(self.domain)
-        self.chrome.execute_script('document.write("{}")'.format(self.htmlcode))
+        try:
+            self.chrome.execute_script('document.write("{}")'.format(self.htmlcode))
+        except selenium.common.exceptions.WebDriverException:
+            pass
         while True:
             if 'Captcha Token Harvester' in self.chrome.page_source:
                 break
             else:
                 pass
         time.sleep(1)
-        self.chrome.execute_script("var evt = document.createEvent('Event');evt.initEvent('load', false, false);window.dispatchEvent(evt);")
+        try:
+            self.chrome.execute_script("var evt = document.createEvent('Event');evt.initEvent('load', false, false);window.dispatchEvent(evt);")
+        except selenium.common.exceptions.WebDriverException:
+            pass
         while True:
             if 'Success' in self.chrome.page_source:
                 break
