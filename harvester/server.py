bf4d8ae01830cb5e7798b814bf1703668f9bb527
Noah Cardoza <noahcardoza@gmail.com>
2020-04-16 15:13:46 -0700

    implemented ExpiringQueue and reworked rest api

--- harvester/server.py
+++ harvester/server.py
@@ -1,29 +1,48 @@
 import ipaddress
 import proxy
-from flask import Flask, jsonify
-import logging
+from flask import Flask, Response, jsonify
+import os
+import threading
+from expiring_queue import ExpiringQueue
 
-logging.getLogger('werkzeug').setLevel(logging.ERROR)
+if os.name != 'nt':  # avoid freeze_support error on Windows
+    import store
+
+tokens = ExpiringQueue(110)
 
 app = Flask(__name__, static_url_path='/static')
 
 
-@app.route('/json', methods=['GET'])
-def json():
-    return jsonify(store.tokens._getvalue())  # pylint: disable=E1101
+@app.route('/tokens', methods=['GET'])
+def tokens_route():
+    return jsonify(list(tokens.queue))
+
+
+@app.route('/token', methods=['GET'])
+def token():
+    if tokens.empty():
+        return Response(status=418)
+    return tokens.get()
 
 
 def start(port=5000, host='127.0.0.1'):
     app.run(host=host, port=port)
 
 
-if __name__ == '__main__':
-    import store  # avoid freeze_support error on Windows
+def queue_worker():
+    while True:
+        tokens.put(store.tokens.get())
 
-    store.host_map[b'sneakersnstuff.com'] = {
+
+if __name__ == '__main__':
+    if os.name == 'nt':  # avoid freeze_support error on Windows
+        import store
+
+    store.host_map['sneakersnstuff.com'] = {
         'type': 'hcaptcha',
         'sitekey': '33f96e6a-38cd-421b-bb68-7806e1764460'
     }
 
+    threading.Thread(target=queue_worker, daemon=True).start()
     with proxy.start(hostname=ipaddress.IPv4Address('127.0.0.1'), plugins='plugin.MyManInTheMiddlePlugin'):
         start()
