9d49e4bfbd3d1bb1026c789e74cc42887668a8d9
Noah Cardoza <noahcardoza@gmail.com>
2020-04-21 02:23:04 -0700

    added more to the help menu

--- harvester/__main__.py
+++ harvester/__main__.py
@@ -1,10 +1,15 @@
 import server
 import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument('type', choices=['recaptcha', 'hcaptcha'])
-ap.add_argument('-k', '--site-key', required=True)
-ap.add_argument('-d', '--domain', required=True)
+ap = argparse.ArgumentParser(
+    description='CaptchaHarvester: Solve captchas yourself without having to pay for services like 2captcha for use in automated projects.',
+    epilog='For help contact @MacHacker#7322 (Discord)')
+ap.add_argument('type', choices=['recaptcha', 'hcaptcha'],
+                help='the type of captcha you are want to solve')
+ap.add_argument('-k', '--site-key', required=True,
+                help='the sitekey used by the captcha on page')
+ap.add_argument('-d', '--domain', required=True,
+                help='the domain for which you want to solve captchas')
 ap.add_argument('-H', '--host', help='defaults to 127.0.0.1',
                 default='127.0.0.1')
 ap.add_argument('-p', '--port', help='defaults to 5000',
