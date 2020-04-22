5767aff29c0e94aef9865318d7c8eb3de53a8ff3
Noah Cardoza <noahcardoza@gmail.com>
2020-04-21 17:18:50 -0700

    now supports windows

--- harvester/browser.py
+++ harvester/browser.py
@@ -8,17 +8,28 @@ browsers = {
 }
 
 
-def launch(pac_script, browser='chrome', restart=False):
+def launch(domain, pac_script, browser='chrome', restart=False):
     user_dir = ''
     app = browsers.get(browser)
     if not app:
         raise ValueError('no configuration for `{}` browser'.format(browser))
 
-    if platform.system() != 'Darwin':
+    system = platform.system()
+    if system == 'Darwin':
+        if not restart:
+            user_dir = '--user-data-dir=/tmp/havester/' + str(uuid4())
+        else:
+            os.system(f'killall "{app}"')
+        os.system(
+            f"open -a '{app}' -n --args --proxy-pac-url='{pac_script}' {user_dir} {domain}")
+    elif system == 'Windows':
+        if not restart:
+            user_dir = '--user-data-dir=' + \
+                os.path.join(os.environ['TEMP'], 'harvester', str(uuid4()))
+        else:
+            os.system(f'TASKKILL /IM {browser}.exe /F')
+        os.system(
+            f'start {browser} --proxy-pac-url="{pac_script}" {user_dir} {domain}')
+    else:
         raise RuntimeError(
-            'automatic broswer functinality only avalible on MacOS')
-
-    if not restart:
-        user_dir = '--user-data-dir=/tmp/havester/' + str(uuid4())
-    os.system(
-        "open -a '{}' -n --args --proxy-pac-url='{}' {}".format(app, pac_script, user_dir))
+            'automatic broswer functinality only avalible on MacOS and Windows for now')
