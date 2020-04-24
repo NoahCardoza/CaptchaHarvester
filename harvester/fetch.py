e0a9544352efd10c26a1ff6c86c50849aaa24684
Noah Cardoza <noahcardoza@gmail.com>
2020-04-24 14:42:30 -0700

    move to plain urllib

--- harvester/fetch.py
+++ harvester/fetch.py
@@ -1,17 +1,15 @@
-import urllib3
+from urllib.request import urlopen, HTTPError
 from time import sleep
 from typing import Tuple
 
 
 def token(server_address: Tuple[str, int], timeout=3) -> str:
     url = f'http://{server_address[0]}:{server_address[1]}/token'
-    http = urllib3.PoolManager()
-    res = http.request('GET', url)
-    while res.status != 200:
-        print(res, res.status, res.data)
-        sleep(timeout)
-        res = http.request('GET', url)
-    return res.data.decode('ascii')
+    while 1:
+        try:
+            return urlopen(url).read().decode('ascii')
+        except HTTPError:
+            sleep(timeout)
 
 
 if __name__ == '__main__':
