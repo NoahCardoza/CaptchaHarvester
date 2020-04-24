32f98b0d1247bac1ed539dc4fbe0e5715b9cfef8
Noah Cardoza <noahcardoza@gmail.com>
2020-04-24 13:10:49 -0700

    added version info

--- harvester/__init__.py
+++ harvester/__init__.py
@@ -2,5 +2,7 @@ from harvester import server, browser, fetch
 from harvester.server import CaptchaKindEnum, tokens
 from harvester.browser import BrowserEnum
 
+__version__ = '1.0.0'
+
 __all__ = ['browser', 'server', 'fetch',
            'tokens', 'CaptchaKindEnum', 'BrowserEnum', ]
