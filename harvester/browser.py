7bc0e82580a57bcbab650e29a7e56686f16b8d50
Noah Cardoza <noahcardoza@gmail.com>
2020-05-07 11:56:39 -0700

    fixes issues/6 and fixes issues/7
    also added option to pass custom executable path and args

--- harvester/browser.py
+++ harvester/browser.py
@@ -2,7 +2,7 @@ import platform
 import os
 from uuid import uuid4
 from enum import Enum
-from typing import Tuple
+from typing import Tuple, List
 
 browsers = {
     'chrome': 'Google Chrome',
@@ -16,7 +16,7 @@ class BrowserEnum(Enum):
 
 
 def launch(domain: str, server_address: Tuple[str, int], browser: BrowserEnum = BrowserEnum.CHROME,
-           restart: bool = False, width: int = 400, height: int = 580):
+           restart: bool = False, width: int = 400, height: int = 580, browser_path: str = None, browser_args: List[str] = []):
     browser = browser.value
     pac_script_url = f'http://{server_address[0]}:{server_address[1]}/{domain}.pac'
     app = browsers.get(browser)
@@ -25,28 +25,33 @@ def launch(domain: str, server_address: Tuple[str, int], browser: BrowserEnum = 
 
     system = platform.system()
     browser_command = []
+
     if system == 'Darwin':
-        browser_command.append(f"open -a '{app}' -n --args")
-        if not restart:
-            browser_command.append(
-                f'--user-data-dir=/tmp/havester/{str(uuid4())}')
-        else:
-            os.system(f'killall "{app}"')
+        browser_path = browser_path or f"open -a '{app}' -n --args"
+        user_data_dir = f'--user-data-dir=/tmp/havester/{str(uuid4())}'
+        restart_command = f'killall "{app}"'
     elif system == 'Windows':
-        if not restart:
-            browser_command.append(
-                f"--user-data-dir={os.path.join(os.environ['TEMP'], 'harvester', str(uuid4()))}")
-        else:
-            os.system(f'TASKKILL /IM {browser}.exe /F')
+        browser_path = browser_path or f"start {browser}"
+        user_data_dir = f"--user-data-dir={os.path.join(os.environ['TEMP'], 'harvester', str(uuid4()))}"
+        restart_command = f'TASKKILL /IM {browser}.exe /F'
     else:
         raise RuntimeError(
             'Automatic broswer functinality only avalible on MacOS and Windows for now')
 
+    browser_command.append(browser_path)
+    if restart:
+        os.system(restart_command)
+    else:
+        browser_command.append(user_data_dir)
+
+    browser_command.extend(browser_args)
     browser_command.extend((
         "--no-default-browser-check",
         f'--proxy-pac-url="{pac_script_url}"',
         f"--window-size={width},{height}",
         f'--app="http://{domain}"'
     ))
+
+    print(browser_command)
 
     os.system(' '.join(browser_command))
