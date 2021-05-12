b595e1ee8d007c79977ca7d71bf376ab400a6284
NoahCardoza <noahcardoza@gmail.com>
2021-05-12 13:02:42 -0700

    bump version for pypi

--- harvester/__init__.py
+++ harvester/__init__.py
@@ -2,7 +2,7 @@ from harvester import browser, fetch, server
 from harvester.browser import BrowserEnum
 from harvester.server import CaptchaKindEnum, Harvester
 
-__version__ = '2.1.1'
+__version__ = '2.2.0'
 
 __all__ = ['browser', 'server', 'fetch',
            'CaptchaKindEnum', 'BrowserEnum', 'Harvester']
